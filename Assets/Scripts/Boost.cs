de6d084ccea40e123bac46312be0215dd63fe042
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-08-24 20:05:24 +0800

    put values for spawning

--- Assets/Scripts/Boost.cs
+++ Assets/Scripts/Boost.cs
@@ -12,12 +12,19 @@ public class Boost : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnBoost", Time.deltaTime, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawBoost()
+    {
+        Vector3 spwanPosition = new Vector3(Random.Range(-spawnAreaSize, spawnHeight), spawnHeight, Random.Range(-spawnAreaSize, spawnAreaSize));
+
+        Instantiate(boostSpawner, spwanPosition, Quaternion.identity);
     }
 }
