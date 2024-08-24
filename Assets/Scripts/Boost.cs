1c7d1beea4417b8ba6e0af3191065dd9661748d1
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-08-24 20:12:31 +0800

    working spawner

--- Assets/Scripts/Boost.cs
+++ Assets/Scripts/Boost.cs
@@ -9,19 +9,20 @@ public class Boost : MonoBehaviour
     public float spawnAreaSize;
     public float spawnHeight;
     public float spawnInterval;
+    public float spawnTime;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnBoost", Time.deltaTime, spawnInterval);
-    }
+		 InvokeRepeating("SpawnBoost",  spawnTime * Time.deltaTime, spawnInterval); // spawn at start
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		// InvokeRepeating("SpawnBoost", 5f, spawnInterval); // spawn every frame
+	}
 
-    void SpawBoost()
+    void SpawnBoost()
     {
         Vector3 spwanPosition = new Vector3(Random.Range(-spawnAreaSize, spawnHeight), spawnHeight, Random.Range(-spawnAreaSize, spawnAreaSize));
 
