84944fd677763a6ae1d4fa868e4b42125d801165
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-08-24 19:49:43 +0800

    spawn script init

--- Assets/Scripts/Boost.cs
+++ Assets/Scripts/Boost.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Boost : MonoBehaviour
 {
+    public GameObject boostSpawner;
+    public float spawnAreaSize;
+    public float spawnHeight;
+    public float spawnInterval;
     // Start is called before the first frame update
     void Start()
     {
